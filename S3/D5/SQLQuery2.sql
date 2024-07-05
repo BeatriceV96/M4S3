64985c1676b3c30b799545714510b07c36660a79
BeatriceV96 <164747965+BeatriceV96@users.noreply.github.com>
2024-07-05 15:39:57 +0200

    Update SQLQuery2.sql

--- S3/D5/SQLQuery2.sql
+++ S3/D5/SQLQuery2.sql
@@ -4,4 +4,4 @@ VALUES
 ('Parcheggio in divieto di sosta', 80.00),
 ('Guida senza cinture', 100.00),
 ('Uso del cellulare alla guida', 200.00),
-('Passaggio con semaforo rosso', 400.00);
+('Passaggio con semaforo rosso', 250.00);
