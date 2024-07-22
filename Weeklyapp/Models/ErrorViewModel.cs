8c9ec71b70a5058f36d96126d39aa0f5c5d8a67d
BeatriceV96 <164747965+BeatriceV96@users.noreply.github.com>
2024-07-22 16:32:06 +0200

    Delete Weeklyapp directory

--- Weeklyapp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Weeklyapp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
